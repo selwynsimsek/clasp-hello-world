d50080e63725007c0c902757de6e0b8740bafd4b
Tarn W. Burton <twburton@gmail.com>
2024-06-10 08:58:07 -0400

    Update startup function name

--- hello-world.cc
+++ hello-world.cc
@@ -3,9 +3,9 @@
 
 namespace hw {
 
-void hello_world(double x, double y) { fmt::print("Hello World! Did you know that {} + {} = {}>?\n", x, y, x + y); }
+void hello_world(double x, double y) { fmt::print("Hello World! Did you know that {} + {} = {}?\n", x, y, x + y); }
 
-extern "C" void clasp_extension_startup() {
+extern "C" void startup_clasp_extension() {
   using namespace clbind;
   fmt::print("Entered {}:{}:{}\n", __FILE__, __LINE__, __FUNCTION__);
   package_ s("HELLO-WORLD", {}, {"COMMON-LISP"});
